3a86443f5d3b1c3eb34d4e2191331411bafa31ad
Jhone <yukiterakunii@gmail.com>
2018-01-30 21:14:59 -0500

    Modelos de datos

---
+++ bd/migration_script.sql
@@ -0,0 +1,16 @@
+-- ----------------------------------------------------------------------------
+-- MySQL Workbench Migration
+-- Migrated Schemata: bpi_erp
+-- Source Schemata: bpi_erp_bd
+-- Created: Mon Jan 29 23:55:56 2018
+-- Workbench Version: 6.3.9
+-- ----------------------------------------------------------------------------
+
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------------------------------------------------------
+-- Schema bpi_erp
+-- ----------------------------------------------------------------------------
+DROP SCHEMA IF EXISTS `bpi_erp` ;
+CREATE SCHEMA IF NOT EXISTS `bpi_erp` ;
+SET FOREIGN_KEY_CHECKS = 1;
